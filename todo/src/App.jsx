f475bd207385aa2d926d677082c935c05202f0c6
Luis Henrique <luishenriqueribeiro3@gmail.com>
2023-08-03 22:14:14 -0300

    criando app react javascript

---
+++ todo/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    const [todos, setTodos] = useState([
+    {
+      id:1,
+      text:"criar funcionalidade x no sistema",
+      
+    }
+    ])
+
+
+<div>
+    Olá React
+</div>;
+  )
+}
+
+export default App
